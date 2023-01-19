c0ab14b74749ad850ead7ed352d2790f80c42b67
SergiusLo <loboyko99911@gmail.com>
2023-01-19 23:53:30 +0200

    store zustand

--- src/data/stores/UseToDoStore.ts
+++ src/data/stores/UseToDoStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { generateId } from "../helpers";
 
 interface Task {
   id: string;
@@ -12,3 +13,33 @@ interface ToDoStore {
   updateTask: (id: string, title: string) => void;
   removeTask: (id: string) => void;
 }
+
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+  tasks: [{ id: "asdasdas", title: "Default task", createAt: 12312 }],
+  createdTask: (title) => {
+    const { tasks } = get();
+    const newTask = {
+      id: generateId(),
+      title,
+      createAt: Date.now(),
+    };
+    set({
+      tasks: [newTask].concat(tasks),
+    });
+  },
+  updateTask: (id: string, title: string) => {
+    const { tasks } = get();
+    set({
+      tasks: tasks.map((task) => ({
+        ...task,
+        title: task.id === id ? title : task.title,
+      })),
+    });
+  },
+  removeTask: (id: string) => {
+    const { tasks } = get();
+    set({
+      tasks: tasks.filter((task) => task.id !== id),
+    });
+  },
+}));
--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./index.module.scss";
+import { useToDoStore } from "../../data/stores/UseToDoStore";
+
 export const App: React.FC = () => {
+  const [tasks, createdTask, updateTask, removeTask] = useToDoStore((state) => [
+    state.tasks,
+    state.createdTask,
+    state.updateTask,
+    state.removeTask,
+  ]);
+
+
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do APP</h1>
