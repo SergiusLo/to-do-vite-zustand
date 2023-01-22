728609ddb3cd205bcd2d2d3760619cc96693b729
SergiusLo <loboyko99911@gmail.com>
2023-01-22 19:18:54 +0200

    inputtask styles

--- src/views/components/InputTask/index.tsx
+++ src/views/components/InputTask/index.tsx
@@ -16,5 +16,38 @@ export const InputTask: React.FC<InputTaskProps> = ({
   onEdited,
   onRemoved,
 }) => {
-  return <div className={styles.inputTask}>{title}</div>;
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div className={styles.inputTask}>
+      <label className={styles.inputTaskLabel}>
+        <input
+          type="checkbox"
+          checked={checked}
+          className={styles.inoutTaskCheckbox}
+          onChange={(evt) => {
+            setChecked(evt.target.checked);
+            if (evt.target.checked) {
+              onDone(id);
+            }
+          }}
+        />
+        <h3 className={styles.inputTaskTitle}>{title}</h3>
+      </label>
+      <button
+        aria-label="Edit"
+        className={styles.inputTaskEdit}
+        onClick={() => {}}
+      />
+      <button
+        aria-label="Remove"
+        className={styles.inputTaskRemove}
+        onClick={() => {
+          if (confirm("Are you sure?")) {
+            onRemoved(id);
+          }
+        }}
+      />
+    </div>
+  );
 };
